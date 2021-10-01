d170a1ffe7bb2f503b242f8e2a1f4e968ebfeaca
Valya Melnychuk <valyameln@gmail.com>
2021-10-01 10:04:55 +0300

    fix popup

--- js/script.js
+++ js/script.js
@@ -2,29 +2,25 @@
 let link = document.querySelector('.header__contacts-feedback');
 let popup = document.querySelector(".modal-content");
 let close = document.querySelector(".modal-content-close");
-let login = popup.querySelector("[name=name]");
+let name = popup.querySelector("[name=name]");
 let form = popup.querySelector("form");
-let password = popup.querySelector("[name=password]");
-let storage = localStorage.getItem("login");
+let email = popup.querySelector("[name=email]");
+let storage = localStorage.getItem("name");
 
 
 link.addEventListener("click", function (event) {
     event.preventDefault();
     popup.classList.add("modal-content-show");
     if (storage) {
-        login.value = storage;
-        password.focus();
-    } else {
-        login.focus();
+        name.value = storage;
+        name.focus();
     }
-    console.log("Клік по силці");
 });
 
 close.addEventListener("click", function (event) {
     event.preventDefault();
     popup.classList.remove('modal-content-show');
     popup.classList.remove('modal-error');
-    console.log("Нажали хрестик");
 });
 
 form.addEventListener("submit", function (event) {
@@ -32,16 +28,15 @@ form.addEventListener("submit", function (event) {
         event.preventDefault();
         popup.classList.add("modal-error")
     } else {
-        localStorage.setItem("login", login.value);
+        localStorage.setItem("name", name.value);
     }
-    console.log("нажавши Війти Перевірили чи заповнені значення форми");
 });
+
 window.addEventListener("keydown", function (event) {
     if (event.keyCode === 27) {
         if (popup.classList.contains("modal-content-show")) {
             popup.classList.remove("modal-content-show");
         }
     }
-    console.log("??????????");
 })
 
